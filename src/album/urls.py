4263574c2f9430f3879ed97470a022215988d8c3
iruma-tea <shusuke.think.pad@gmail.com>
2024-02-08 21:29:55 +0900

    tagの絞り込みページの作成 djangoプラグインの設定を追加

--- src/album/urls.py
+++ src/album/urls.py
@@ -3,10 +3,11 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
-from .views import PhotoListView, PhotoDetailView
+from .views import PhotoListView, PhotoDetailView, TagPhotoListView
 
 urlpatterns = [
     path("", PhotoListView.as_view(), name="photo-list"),
+    path("tag/<str:tag>", TagPhotoListView.as_view(), name="tag-photo-list"),
     path("photo/<int:pk>", PhotoDetailView.as_view(), name="photo-detail"),
 ]
 
--- src/album/views.py
+++ src/album/views.py
@@ -1,5 +1,6 @@
+from django.shortcuts import get_object_or_404
 from django.views.generic import ListView, DetailView
-from .models import Photo
+from .models import Tag, Photo
 
 # Create your views here.
 
@@ -14,3 +15,14 @@ class PhotoDetailView(DetailView):
     model = Photo
     template_name = "album/photo_detail.html"
     context_object_name = "photo"
+
+
+class TagPhotoListView(ListView):
+    model = Photo
+    template_name = "album/tag_photo.html"
+    context_object_name = "photos"
+
+    def get_queryset(self, **kwargs):
+        tag_name = self.kwargs["tag"]
+        tag = get_object_or_404(Tag, name=tag_name)
+        return super().get_queryset().filter(tags=tag)
