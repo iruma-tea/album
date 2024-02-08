37aaf01ec44b8201e93d50a09442180ad5baf4ca
iruma-tea <shusuke.think.pad@gmail.com>
2024-02-08 20:31:48 +0900

    詳細ページを作成

--- src/album/urls.py
+++ src/album/urls.py
@@ -3,10 +3,11 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
-from .views import PhotoListView
+from .views import PhotoListView, PhotoDetailView
 
 urlpatterns = [
-    path("", PhotoListView.as_view(), name="photo-list")
+    path("", PhotoListView.as_view(), name="photo-list"),
+    path("photo/<int:pk>", PhotoDetailView.as_view(), name="photo-detail"),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- src/album/views.py
+++ src/album/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Photo
 
 # Create your views here.
@@ -8,3 +8,9 @@ class PhotoListView(ListView):
     model = Photo
     template_name = "album/photo_list.html"
     context_object_name = "photos"
+
+
+class PhotoDetailView(DetailView):
+    model = Photo
+    template_name = "album/photo_detail.html"
+    context_object_name = "photo"
